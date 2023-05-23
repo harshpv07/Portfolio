4940f417c494a93fc2ecacbe5b23b7607a980d17
harshpv07 <harsh.pv07@gmail.com>
2023-05-24 00:38:31 +0530

    CSS changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'fontawesomefree',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'IN'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
